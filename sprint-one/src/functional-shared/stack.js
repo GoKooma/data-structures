e343add8779eb37097293ca0df1462d7490c08f5
KweonMo Koo <mikekoo2010@gmail.com>
2019-01-04 15:06:33 -0800

    Completed stacks.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not to reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.counter = 0;
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter += 1;
+  },
+
+  size: function() {
+    return this.counter;
+  },
+
+  pop: function() {
+    if (this.counter > 0) {
+      this.counter -= 1;
+      let temp = this.storage[this.counter];
+      delete this.storage[this.counter];
+      return temp;
+    }
+  }
+};
+
+// stackMethods.push = function(value) {
+//   this.storage[counter] = value;
+//   this.storage.counter += 1;
+//   return this.storage;
+// }
+
+// stackMethods.size = function() {
+//   return this.counter;
+// }
 
 
