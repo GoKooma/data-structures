638c1bf59c4b7f727f00baf4a0f3317e29983d47
KweonMo Koo <mikekoo2010@gmail.com>
2019-01-04 16:18:50 -0800

    Completed prototypal inheritence

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.storage = {};
+  newInstance.counter = 0;
+
+  return newInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.counter] = value;
+  this.counter += 1;
+}
 
+queueMethods.dequeue = function() {
+  if (this.counter > 0) {
+    let temp = this.storage[0];
+    delete this.storage[0];
+    for (let i = 0; i < this.counter; i++) {
+      if (this.storage[i + 1]) {
+        this.storage[i] = this.storage[i + 1]
+      }
+    }
+    this.counter -= 1;
+    return temp;
+  }
+}
+
+queueMethods.size = function() {
+  return this.counter;
+}
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.counter = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.counter] = value;
+  this.counter += 1;
+}
 
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    this.counter -= 1;
+    let temp = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return temp;
+  }
+}
+
+stackMethods.size = function() {
+  return this.counter;
+}
