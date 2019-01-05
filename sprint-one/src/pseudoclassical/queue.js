dfde2856ba9b36f72b956a194dc15c9012283957
KweonMo Koo <mikekoo2010@gmail.com>
2019-01-04 16:28:30 -0800

    Completed pseudoclassical inheritence

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.counter] = value;
+  this.counter += 1;
+}
 
+Queue.prototype.dequeue = function() {
+  if (this.counter > 0) {
+    let temp = this.storage[0];
+    delete this.storage[0];
+    for (let i = 0; i < this.counter; i++) {
+      if (this.storage[i + 1]) {
+        this.storage[i] = this.storage[i + 1];
+      }
+    }
+    this.counter -= 1;
+    return temp;
+  }
+}
+
+Queue.prototype.size = function() {
+  return this.counter;
+}
